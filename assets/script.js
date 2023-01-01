e518f07fafaeb14daf11bca0b4a33d9b683b4d17
Ryebread5555 <Rypetty55@gmail.com>
2023-01-01 15:51:21 -0600

    changed header color and added eventlistners/local

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,21 @@
 // Displays the current day and date
-var todayDate = dayjs().format('dddd, MMM D YYYY');
-$("#currentDay").html(todayDate);
+var today = dayjs();
+$('#currentDay').text(today.format('dddd, MMM D, YYYY'));
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+$(document).ready(function () {
+  // event listener for save buttons
+  $(".saveBtn").on("click", function(){
+    // console.log('button has been pressed')
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-
-$(function () {
+    //saving daily planner text into local storage
+    localStorage.setItem(time, text);
+  })
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
