2f31de47086b4c6ccba76b9236e0ae22e96b61af
Ryebread5555 <Rypetty55@gmail.com>
2023-01-02 16:42:13 -0600

    updated hours to 9-5 business hours

--- assets/script.js
+++ assets/script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
     })
   }
   // grabs saved items in localStorage and puts in text area description. Sets the values of each hour.
-  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -51,12 +51,6 @@ $(document).ready(function () {
   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-  $("#hour-18 .description").val(localStorage.getItem("hour-18"));
-  $("#hour-19 .description").val(localStorage.getItem("hour-19"));
-  $("#hour-20 .description").val(localStorage.getItem("hour-20"));
-  $("#hour-21 .description").val(localStorage.getItem("hour-21"));
-  $("#hour-22 .description").val(localStorage.getItem("hour-22"));
-  $("#hour-23 .description").val(localStorage.getItem("hour-23"));
 
   // functions being called
   trackedEvent();
